fb0eb9045e6f98ad72044ae74e8a6627d85d1146
Jacob Graham <jakegraham54@gmail.com>
2022-02-16 22:13:45 -0500

    Created new Activity which allows a user to submit mock info into a Firebase database

---
+++ app/src/main/java/com/jacobdgraham/comp3025timetrackerkotlin/CreateProjectActivity.kt
@@ -0,0 +1,43 @@
+package com.jacobdgraham.comp3025timetrackerkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.jacobdgraham.comp3025timetrackerkotlin.databinding.ActivityCreateProjectBinding
+
+class CreateProjectActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCreateProjectBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreateProjectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPushProjectToDatabase.setOnClickListener {
+            val projectName = binding.editTextProjectName.text.toString().trim()
+            val projectDescription = binding.editTextProjectDescription.text.toString().trim()
+
+            if (projectName.isNotEmpty() && projectDescription.isNotEmpty()) {
+                var project = Project(projectName, projectDescription)
+
+                // Get instance of a Firebase database and connect to an existing collection
+                val databaseInstance = FirebaseFirestore.getInstance().collection("projects")
+
+                // Fetch unique id from Firestore for the insertion to collection
+                val uniqueId = databaseInstance.document().getId()
+
+                databaseInstance.document(uniqueId).set(project).addOnSuccessListener {
+                    Toast.makeText(this, "Firebase database updated with new project", Toast.LENGTH_LONG).show()
+                }
+                    .addOnFailureListener {
+                        exception -> Log.w("Firebase database issue", exception.localizedMessage)
+                    }
+            } else {
+                Toast.makeText(this, "Project name and description must be filled in", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jacobdgraham/comp3025timetrackerkotlin/Project.kt
@@ -0,0 +1,3 @@
+package com.jacobdgraham.comp3025timetrackerkotlin
+
+data class Project(var projectName: String? = null, var description: String? = null, var id:String? = null)
