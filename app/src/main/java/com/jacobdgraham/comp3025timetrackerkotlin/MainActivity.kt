7fdc4fb4c863430c46ac2a569cafa9da263c9d75
Jacob Graham <jakegraham54@gmail.com>
2022-02-16 20:43:00 -0500

    Added boilerplate Gradle android project folder structure. Dependencies and packages were added to enable a connection to a Firebase NoSQL database.

---
+++ app/src/main/java/com/jacobdgraham/comp3025timetrackerkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jacobdgraham.comp3025timetrackerkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
